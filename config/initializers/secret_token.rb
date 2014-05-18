30c8f72fd7f907f2370180ff6d61d9f78570e304
Andrew Backes <backes.andrew@gmail.com>
2014-05-17 20:10:22 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreditForCredits::Application.config.secret_key_base = '66510a68e056331b86fff4deb10c20acec148e5f621d9e07b9414f7b1553c518a44f295014def8107a689d7f69dddc3881b2b36de259db13d97e9b6cfaf77daf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CreditForCredits::Application.config.session_store :cookie_store, key: '_CreditForCredits_session'
